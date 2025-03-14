e44a6173967d8446071a093aaece4a184781c060
Gerardo <the.z0w@gmail.com>
2025-03-14 15:25:42 -0600

    fdf

--- tv-dashboard/scripts.js
+++ tv-dashboard/scripts.js
@@ -1,31 +1,33 @@
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", () => {
   // Obtener los datos de docks
-  $.ajax({
-    url: "../api/get_docks.php",
-    type: "GET",
-    success: function (data) {
-      const docks = JSON.parse(data);
-      displayDocks(docks);
-    },
-  });
+  fetch("../api/get_docks.php")
+    .then((response) => response.json())
+    .then((data) => {
+      displayDocks(data);
+    })
+    .catch((error) => console.error("Error al obtener los datos:", error));
 
   // Función para mostrar los datos de docks
   function displayDocks(docks) {
-    let html = "";
+    const dockDataContainer = document.getElementById("dockData");
+    dockDataContainer.innerHTML = ""; // Limpiar el contenedor
+
     docks.forEach((dock) => {
-      html += `
-                <div class="col-md-4">
-                    <div class="card">
-                        <img src="${dock.image}" class="card-img-top" alt="Dock Image">
-                        <div class="card-body">
-                            <h5 class="card-title">${dock.name}</h5>
-                            <p class="card-text">Estado: ${dock.status}</p>
-                            <p class="card-text">Fecha: ${dock.date}</p>
-                        </div>
+      const dockCard = document.createElement("div");
+      dockCard.classList.add("col-md-4");
+
+      dockCard.innerHTML = `
+                <div class="card">
+                    <img src="${dock.image}" class="card-img-top" alt="Dock Image">
+                    <div class="card-body">
+                        <h5 class="card-title">${dock.name}</h5>
+                        <p class="card-text">Estado: ${dock.status}</p>
+                        <p class="card-text">Fecha: ${dock.date}</p>
                     </div>
                 </div>
             `;
+
+      dockDataContainer.appendChild(dockCard);
     });
-    $("#dockData").html(html);
   }
 });
