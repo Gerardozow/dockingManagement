e858001867c4fb760ae3b29d095d7582ab1ead3c
Gerardo Reyes <the.z0w@gmail.com>
2025-03-15 09:32:06 -0600

    correccion de margin en username

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,25 +1,25 @@
 <?php if (isset($_SESSION['user_id'])): ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <i class="bi bi-box-seam"></i> Gestión Docks
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <div class="navbar-nav ms-auto">
-                <?php if (getUserRole() === 'admin'): ?>
-                <span class="nav-item me-3 text-warning">
-                    <i class="bi bi-shield-lock"></i> Admin
-                </span>
-                <?php endif; ?>
-                <a class="nav-link" href="logout.php">
-                    <i class="bi bi-box-arrow-right"></i>
-                    <span class="d-lg-none">Salir</span>
-                </a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <i class="bi bi-box-seam"></i> Gestión Docks
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav ms-auto">
+                    <?php if (getUserRole() === 'admin'): ?>
+                        <span class="nav-item me-3 text-warning username">
+                            <i class="bi bi-shield-lock"></i> Admin
+                        </span>
+                    <?php endif; ?>
+                    <a class="nav-link" href="logout.php">
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span class="d-lg-none">Salir</span>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
-</nav>
+    </nav>
 <?php endif; ?>
